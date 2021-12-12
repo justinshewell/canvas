f61ddfa59420a06976514bceaab722cdd685ac74
justinshewell <96019399+justinshewell@users.noreply.github.com>
2021-12-12 12:05:19 -0700

    Update scroll_quiz.js

--- scroll_quiz.js
+++ scroll_quiz.js
@@ -38,68 +38,76 @@
               else title.text($title);
           }
       }));
-      $('#justin-buttons').remove();
-      $('body').append('<div id="justin-buttons" data-qnum="-1"><small>Click the title bar above to drag this box anywhere on the screen.<br><br>These buttons scroll to questions that are marked incorrect or ungraded that are not multiple-choice, true/false, or matching questions, allowing you to easily review questions for grading without having to scroll yourself.</small><p style="text-align: center; margin-top: 20px;"><button id="justin-first" disabled>&lt;&lt; First</button> &nbsp; &nbsp; <button id="justin-previous" disabled>&lt; Previous</button> &nbsp; &nbsp; <button id="justin-next">Next &gt;</button> &nbsp; &nbsp; <button id="justin-last">Last &gt;&gt;</button></p></div>');
-      $('#justin-buttons').dialog({
-          height: 240,
-          width: 450,
-          title: 'Scroll Quiz Questions',
-          titleIsHtml: true,
-          resizable: true,
-          draggable: true,
-          dialogClass: 'alertDialog',
-          modal: false,
-          show: {
-              effect: 'shake',
-              duration: 1000
-          }
-      });
-      $('#justin-first').on('click', function() {
-          var iframe = $('#speedgrader_iframe');
-          var curQuestion = 0;
-          var top = $(iframe).contents().find('.question:not(.matching_question, .multiple_choice_question, .true_false_question, .correct)').first().offset().top;
-          $(iframe).contents().children().animate({scrollTop: top}, 500);
+      var buttons = $('#justin-buttons');
+      if(buttons.length > 0) { // already exists, so update
+          $('#justin-buttons').attr('data-qnum', '-1');
+          $('#justin-first, #justin-previous').prop('disabled', true);
           $('#justin-next, #justin-last').prop('disabled', false);
-          $('#justin-previous, #justin-first').prop('disabled', true);
-          $('#justin-buttons').attr('data-qnum', curQuestion);
-      });
-      $('#justin-next').on('click', function() {
-          var iframe = $('#speedgrader_iframe');
-          var curQuestion = $('#justin-buttons').attr('data-qnum');
-          console.log(curQuestion);
-          var els = $(iframe).contents().find('.question:not(.matching_question, .multiple_choice_question, .true_false_question, .correct)');
-          console.log(els);
-          curQuestion++;
-          if(curQuestion >= els.length) return false;
-          if(curQuestion > 0) $('#justin-first, #justin-previous').prop('disabled', false);
-          if(curQuestion == els.length - 1) $('#justin-next, #justin-last').prop('disabled', true);
-          var top = $(els[curQuestion]).offset().top;
-          console.log(top);
-          $(iframe).contents().children().animate({scrollTop: top}, 500);
-          $('#justin-buttons').attr('data-qnum', curQuestion);
-      });
-      $('#justin-previous').on('click', function() {
-          var iframe = $('#speedgrader_iframe');
-          var curQuestion = $('#justin-buttons').attr('data-qnum');
-          var els = $(iframe).contents().find('.question:not(.matching_question, .multiple_choice_question, .true_false_question, .correct)');
-          curQuestion--;
-          if(curQuestion < 0) return false;
-          if(curQuestion == 0) $('#justin-first, #justin-previous').prop('disabled', true);
-          if(curQuestion < els.length - 1) $('#justin-next, #justin-last').prop('disabled', false);
-          var top = $(els[curQuestion]).offset().top;
-          $(iframe).contents().children().animate({scrollTop: top}, 500);
-          $('#justin-buttons').attr('data-qnum', curQuestion);
-      });
-      $('#justin-last').on('click', function() {
-          var iframe = $('#speedgrader_iframe');
-          var els = $(iframe).contents().find('.question:not(.matching_question, .multiple_choice_question, .true_false_question, .correct)');
-          var curQuestion = $('#justin-buttons').attr('data-qnum');
-          curQuestion = els.length - 1;
-          var top = $(els[curQuestion]).offset().top;
-          $(iframe).contents().children().animate({scrollTop: top}, 500);
-          $('#justin-buttons').attr('data-qnum', curQuestion);
-          $('#justin-next, #justin-last').prop('disabled', true);
-          $('#justin-previous, #justin-first').prop('disabled', false);
-      });
+      } else {
+          $('body').append('<div id="justin-buttons" data-qnum="-1"><small>Click the title bar above to drag this box anywhere on the screen.<br><br>These buttons scroll to questions that are marked incorrect or ungraded that are not multiple-choice, true/false, or matching questions, allowing you to easily review questions for grading without having to scroll yourself.</small><p style="text-align: center; margin-top: 20px;"><button id="justin-first" disabled>&lt;&lt; First</button> &nbsp; &nbsp; <button id="justin-previous" disabled>&lt; Previous</button> &nbsp; &nbsp; <button id="justin-next">Next &gt;</button> &nbsp; &nbsp; <button id="justin-last">Last &gt;&gt;</button></p></div>');
+          $('#justin-buttons').dialog({
+              height: 240,
+              width: 450,
+              title: 'Scroll Quiz Questions',
+              titleIsHtml: true,
+              resizable: true,
+              draggable: true,
+              dialogClass: 'alertDialog',
+              modal: false,
+              show: {
+                  effect: 'shake',
+                  duration: 1000
+              }
+          });
+          $('#justin-first').on('click', function() {
+              var iframe = $('#speedgrader_iframe');
+              var curQuestion = 0;
+              var top = $(iframe).contents().find('.question:not(.matching_question, .multiple_choice_question, .true_false_question, .correct)').first().offset().top;
+              $(iframe).contents().children().animate({scrollTop: top}, 500);
+              $('#justin-next, #justin-last').prop('disabled', false);
+              $('#justin-previous, #justin-first').prop('disabled', true);
+              $('#justin-buttons').attr('data-qnum', curQuestion);
+          });
+          $('#justin-next').on('click', function() {
+              var iframe = $('#speedgrader_iframe');
+              var curQuestion = $('#justin-buttons').attr('data-qnum');
+              console.log(curQuestion);
+              var els = $(iframe).contents().find('.question:not(.matching_question, .multiple_choice_question, .true_false_question, .correct)');
+              console.log(els);
+              curQuestion++;
+              if(curQuestion >= els.length) return false;
+              if(curQuestion > 0) $('#justin-first, #justin-previous').prop('disabled', false);
+              if(curQuestion == els.length - 1) $('#justin-next, #justin-last').prop('disabled', true);
+              var top = $(els[curQuestion]).offset().top;
+              console.log(top);
+              $(iframe).contents().children().animate({scrollTop: top}, 500);
+              $('#justin-buttons').attr('data-qnum', curQuestion);
+          });
+          $('#justin-previous').on('click', function() {
+              var iframe = $('#speedgrader_iframe');
+              var curQuestion = $('#justin-buttons').attr('data-qnum');
+              var els = $(iframe).contents().find('.question:not(.matching_question, .multiple_choice_question, .true_false_question, .correct)');
+              curQuestion--;
+              if(curQuestion < 0) return false;
+              if(curQuestion == 0) $('#justin-first, #justin-previous').prop('disabled', true);
+              if(curQuestion < els.length - 1) $('#justin-next, #justin-last').prop('disabled', false);
+              var top = $(els[curQuestion]).offset().top;
+              $(iframe).contents().children().animate({scrollTop: top}, 500);
+              $('#justin-buttons').attr('data-qnum', curQuestion);
+          });
+          $('#justin-last').on('click', function() {
+              var iframe = $('#speedgrader_iframe');
+              var els = $(iframe).contents().find('.question:not(.matching_question, .multiple_choice_question, .true_false_question, .correct)');
+              var curQuestion = $('#justin-buttons').attr('data-qnum');
+              curQuestion = els.length - 1;
+              var top = $(els[curQuestion]).offset().top;
+              $(iframe).contents().children().animate({scrollTop: top}, 500);
+              $('#justin-buttons').attr('data-qnum', curQuestion);
+              $('#justin-next, #justin-last').prop('disabled', true);
+              $('#justin-previous, #justin-first').prop('disabled', false);
+          });
+      }
   }
 })();
+
+
